d565f5085a8e5d29f4b270defb866bc316a2514e
Reto Wettstein <retowettstein@hotmail.com>
2019-09-28 19:01:14 +0200

    more model data

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,10 +1,25 @@
 from app import app
-from datahandler import data_handler
 from flask import Response
 import json
 import pickle
+import pandas as pd
+import numpy as np
+from model import analysis
 
-from model.analysis import getEvents
+
+@app.before_first_request
+def loadAll():
+    global data
+    data = analysis.loadData()
+
+    global cleanedData
+    cleanedData = analysis.cleanData(data)
+
+    global transformedData
+    transformedData = analysis.transformData(cleanedData)
+
+    global mappings
+    mappings = analysis.loadMapping()
 
 
 @app.route("/")
@@ -12,34 +27,35 @@ def hello():
     return "Hello World!"
 
 
-@app.before_first_request
-def loadAllCsv():
-    global data
-    data = data_handler.loadMeasures()
-
-    global mappings
-    mappings = data_handler.loadMappings()
-
-
 @app.route('/averages', methods=['GET'])
 def averages():
-    cleanedData = data_handler.cleanData(data)
-    responseData = data_handler.calculateAverageFromMeasures(cleanedData)
+    responseData = analysis.calculateAverages(cleanedData)
 
     responseData.set_index("uniqueId", inplace=True)
-    responseData.loc[:, "coordinates"] = data_handler.getCoordinates(responseData.index, mappings)
+    responseData.loc[:, "coordinates"] = analysis.getCoordinates(responseData.index, mappings)
     responseData.reset_index(inplace=True)
 
     return Response(responseData.to_json(orient='records'), mimetype="application/json")
 
 
-@app.route("/predict/<date>", methods=['GET'])
-def predicte(date):
-    with open('../data/transformed.p', 'rb') as file:
+@app.route("/predict/<date>/<daySegment>", methods=['GET'])
+def predict(date, daySegment):
+    with open('../data/model.p', 'rb') as file:
         model = pickle.load(file)
+
+    matchingData = cleanedData[cleanedData["date"] == pd.to_datetime(date).date()]
+    matchingData = matchingData.loc[matchingData["time"] == daySegment]
+
+    Xcategories = matchingData[['place_type', 'weekday', 'time', 'isHoliday', 'weatherCat']]
+    features = pd.get_dummies(Xcategories, columns=['place_type', 'weekday', 'time', 'weatherCat'])
+
+    predictions = model.predict(features)
+    matchingData["cci"] = predictions
+
+    return Response(matchingData.to_json(orient='records'), mimetype="application/json")
 
 
 @app.route("/events/<date>", methods=['GET'])
 def event(date):
-    eventsInBasel = getEvents(date=date)
+    eventsInBasel = analysis.getEvents(date=date)
     return Response(json.dumps(eventsInBasel), mimetype="application/json")
