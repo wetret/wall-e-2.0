3a1c4ee47371d987d8124d87358cfc3bee6cd652
Gwendolyn Rippberger <gwendolyn.rippberger@gmail.com>
2019-09-28 09:01:41 +0200

    working map

--- garbage-map/src/app/app.component.ts
+++ garbage-map/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare var ol: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'garbage-map';
+
+  atitude = 18.5204;
+  longitude = 73.8567;
+
+  map: any;
+
+  ngOnInit() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([73.8567, 18.5204]),
+        zoom: 8
+      })
+    });
+
+  }
 }
--- garbage-map/src/app/map-view/map-view.component.ts
+++ garbage-map/src/app/map-view/map-view.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
+declare var ol: any;
 
 @Component({
   selector: 'app-map-view',
@@ -14,9 +9,24 @@ export interface Tile {
 })
 export class MapViewComponent implements OnInit {
 
-  constructor() { }
+  latitude = 18.5204;
+  longitude = 73.8567;
+
+  map: any;
 
   ngOnInit() {
-  }
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([73.8567, 18.5204]),
+        zoom: 8
+      })
+    });
 
+  }
 }
