ca49853317f24ce2214cab3b2e7e4176d21349ba
Maximilian Söchting <maximilian.soechting@student.hpi.de>
2019-09-28 09:35:18 +0200

    canada example working with basic map layer

--- garbage-map/src/app/app.component.ts
+++ garbage-map/src/app/app.component.ts
@@ -7,27 +7,48 @@ declare var ol: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'garbage-map';
 
-  atitude = 18.5204;
-  longitude = 73.8567;
+  initialLatitude = 7.5868;
+  initialLongitude = 47.5585;
 
   map: any;
+  vectorLines: any;
 
   ngOnInit() {
+    this.vectorLines = new ol.layer.Vector({
+      source: new ol.source.Vector({
+        url: 'https://openlayers.org/en/latest/examples/data/geojson/polygon-samples.geojson',
+        format: new ol.format.GeoJSON()
+      }),
+      style: lineStyleFunction
+    });
+
+    function lineStyleFunction(feature, resolution) {
+      return new ol.style.Style({
+        stroke: new ol.style.Stroke({
+          color: 'green',
+          width: 2
+        }),
+        text: null
+      });
+    }
+
     this.map = new ol.Map({
       target: 'map',
       layers: [
         new ol.layer.Tile({
           source: new ol.source.OSM()
-        })
+        }),
+        this.vectorLines
       ],
       view: new ol.View({
-        center: ol.proj.fromLonLat([73.8567, 18.5204]),
+        /*center: ol.proj.fromLonLat([this.initialLatitude, this.initialLongitude]),
+        zoom: 15*/
+        center: [-8161939, 6095025],
         zoom: 8
       })
     });
-
   }
 }
