f5525223c2e4945293941a55c27b23843a98654d
Maximilian Söchting <maximilian.soechting@student.hpi.de>
2019-09-29 04:19:31 +0200

    averages!!!

---
+++ garbage-map/src/app/averageinfo.interface.ts
@@ -0,0 +1,16 @@
+
+export interface AverageInfo {
+  name: string;
+  type: string;
+  cci: number;
+
+  rateGums: number;
+  rateSyringues: number;
+  rateLeaves: number;
+  rateGlassDebris: number;
+  rateBottles: number;
+  ratePapers: number;
+  rateExcrements: number;
+  rateCigarrettes: number;
+  rateGrits: number;
+}
